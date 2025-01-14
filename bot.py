3e214305fdf9db1264519071c0c65073b9e9cf6f
rohit3607 <rohitplayer87089@gmail.com>
2025-01-14 21:04:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,6 @@ from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
 
 class Bot(Client):
-
     def __init__(self):
         super().__init__(
             "techvj login",
@@ -18,19 +17,15 @@ class Bot(Client):
             sleep_threshold=10
         )
 
-      
-    async def start(self):
-            
-        await super().start()
+    async def start(self, *args, **kwargs):  # Accept additional arguments
+        await super().start(*args, **kwargs)  # Pass them to the parent start()
         print('Bot Started Powered By @VJ_Botz')
 
-    async def stop(self, *args):
-
-        await super().stop()
+    async def stop(self, *args):  # Accept additional arguments if needed
+        await super().stop(*args)
         print('Bot Stopped Bye')
 
-if __name__ == "__main__":
-    Bot().run()
+Bot().run()
 
 # Don't Remove Credit Tg - @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
